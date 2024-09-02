c7f741fd8d87fa8e6d39f526577cbf12785fe29a
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-09-02 15:04:55 -0700

    completed hero section

--- learn-react/airbnb-clone/src/components/Hero.js
+++ learn-react/airbnb-clone/src/components/Hero.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import heroImage from '../images/photo-grid.png'
 
 const Hero = () => {
   return (
-    <div>
-        Hero
-    </div>
+    <section className='hero'>
+        <img src={heroImage} className='hero--photo'/>
+        <h1 className='hero--header'>Online Experiences</h1>
+        <p className='hero--text'>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+    </section>
   )
 }
 
