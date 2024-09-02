324c811dd0d1aab748567a03d28cbfe0a241c28f
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-09-02 16:01:53 -0700

    dynamically added all the information for the card components by passing in props

--- learn-react/airbnb-clone/src/App.js
+++ learn-react/airbnb-clone/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Card from './components/Card';
+import data from './data'
+
 function App() {
+
+  const cards = data.map(({id, title, description, price, coverImg, stats, location}) => 
+    <Card 
+      id={id}
+      title={title}
+      description={description}
+      price={price}
+      coverImg={coverImg}
+      stats={stats}
+      location={location}
+    />
+  )
+  
   return (
     <div className="App">
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      {cards}
+      
     </div>
   );
 }
--- learn-react/airbnb-clone/src/components/Card.js
+++ learn-react/airbnb-clone/src/components/Card.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Card = () => {
+import star from '../images/star.png'
+const Card = ({id, title, description, price, coverImg, stats, location}) => {
+  const {rating, reviewCount} = stats
   return (
-    <div>
-        
+    <div className='card' key={id}>
+        <img src={coverImg} className='card--image'/>
+        <div className='card--stats'>
+            
+            <span className='gray'>{rating} ({reviewCount}) • </span>
+            <span className='gray'>USA</span>
+        </div>
+        <p>{title}</p>
+        <p><span className='bold'>From ${price}</span> / person</p>
     </div>
   )
 }
